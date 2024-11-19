071329583067b4780a4d5d5db4bbdbbb76a54680
kionys <kiwon7271@naver.com>
2024-11-19 22:23:10 +0900

    update: v-for example

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,38 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+</script>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      menus: ["Home", "Shop", "About"],
+      products: [
+        { name: "역삼동원룸", value: 50 },
+        { name: "천호동원룸", value: 30 },
+        { name: "마포구원룸", value: 20 },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="menu">
+    <a href="" v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+  </div>
+  <ul>
+    <li v-for="(product, index) in products" :key="index">{{ product.name }} - {{ product.value }}만원</li>
+  </ul>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
